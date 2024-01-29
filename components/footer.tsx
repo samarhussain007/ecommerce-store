b786ba4f3f9f79d4133e9409ab1c68c74c5f5ff8
MoonstarDev <admin.development@gameduels.in>
2024-01-30 00:34:51 +0530

    first commit

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white border-1">
+      <div className="mx-auto py-18">
+        <p className="text-center text-xs text-black">
+          &copy; 2023 SamarStore, Inc All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
