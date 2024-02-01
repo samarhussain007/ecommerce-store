b3f2565bf498b6c51ae1499cc1be686894e20bb2
MoonstarDev <admin.development@gameduels.in>
2024-02-02 02:29:17 +0530

    created the category section with the filters section

--- components/ui/Billboard.tsx
+++ components/ui/Billboard.tsx
@@ -5,7 +5,7 @@ interface BillboardProps {
 }
 
 const Billboard: React.FC<BillboardProps> = ({ data }) => {
-  console.log(data);
+  console.log(data?.imageUrl);
   return (
     <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
       <div className="rounded-xl aspect-square relative md:aspect-[2.4/1] overflow-hidden bg-cover items-center justify-center">
--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -6,7 +6,7 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, children, disabled, type = "button" }, ref) => {
+  ({ className, children, disabled, type = "button", ...props }, ref) => {
     // Function body
     return (
       <button
@@ -15,6 +15,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
           className
         )}
         ref={ref}
+        {...props}
       >
         {children}
       </button>
