85cb58b722ac04fb5309aa99942d46fed0baadb3
MoonstarDev <admin.development@gameduels.in>
2024-02-03 01:34:16 +0530

    preview modal for individual product completed

--- components/ui/ProductCard.tsx
+++ components/ui/ProductCard.tsx
@@ -7,6 +7,9 @@ import IconButton from "./IconButton";
 import { Product } from "@/types";
 import Currency from "./Currency";
 import { useRouter } from "next/navigation";
+import { MouseEventHandler } from "react";
+import PreviewModal from "./PreviewModal";
+import usePreviewModal from "@/hooks/use-preview-modal";
 
 interface ProductCard {
   data: Product;
@@ -15,6 +18,13 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
   const router = useRouter();
   const handleClick = () => {
     router.push(`/product/${data?.id}`);
+  };
+  const previewModal = usePreviewModal();
+
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    previewModal.onOpen(data);
   };
   return (
     <div
@@ -31,7 +41,7 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
             <IconButton
-              onClick={() => {}}
+              onClick={onPreview}
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
---
+++ providers/modal-providers.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import PreviewModal from "@/components/ui/PreviewModal";
+
+const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  return (
+    <>
+      <PreviewModal />
+    </>
+  );
+};
+
+export default ModalProvider;
