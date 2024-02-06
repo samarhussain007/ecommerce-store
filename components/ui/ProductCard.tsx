4ba285b06eef64892ff206c2787c677c17972fc3
MoonstarDev <admin.development@gameduels.in>
2024-02-07 02:11:02 +0530

    added checkout page

--- components/ui/ProductCard.tsx
+++ components/ui/ProductCard.tsx
@@ -10,6 +10,7 @@ import { useRouter } from "next/navigation";
 import { MouseEventHandler } from "react";
 import PreviewModal from "./PreviewModal";
 import usePreviewModal from "@/hooks/use-preview-modal";
+import useCart from "@/hooks/use-cart";
 
 interface ProductCard {
   data: Product;
@@ -20,11 +21,18 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
     router.push(`/product/${data?.id}`);
   };
   const previewModal = usePreviewModal();
+  const cart = useCart();
 
   const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
     event.stopPropagation();
 
     previewModal.onOpen(data);
+  };
+
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    cart.addItem(data);
   };
   return (
     <div
@@ -45,7 +53,7 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
-              onClick={() => {}}
+              onClick={onAddToCart}
               icon={<ShoppingCart size={20} className="text-gray-600" />}
             />
           </div>
